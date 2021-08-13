b9b809a6d476b8a751e4a816a2e921ecfa106b00
rslewenstein <rafasoaresjf@gmail.com>
2021-08-13 20:58:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace bot_csharp_telegram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ExemploMeuBotCsharp_bot
+            // 1945716668:AAHXhsfcBRd-vYtqpQtCKVJSI6ZkRlkaUcs
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
