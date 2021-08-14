f286b411381d70328886414d85bf6e9f936ed7a2
rslewenstein <rafasoaresjf@gmail.com>
2021-08-13 21:34:54 -0300

    Primeiras implementaçoes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+//using Telegram.Bot.Extensions.Polling;
 
 namespace bot_csharp_telegram
 {
@@ -10,6 +11,30 @@ namespace bot_csharp_telegram
         static void Main()
         {
             botClient = new TelegramBotClient("YOUR_TOKEN");
+
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine($"Olá! Eu sou o {me.FirstName}.");
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+
+            botClient.StopReceiving();
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Mensagem recebida do chat {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(
+                    chatId: e.Message.Chat,
+                    text: "Você disse:\n " + e.Message.Text
+                );
+            }
         }
     }
 }
