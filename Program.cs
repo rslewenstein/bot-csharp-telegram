96391b98b11b71d18e0d1a59e72be31258c16764
rslewenstein <rafasoaresjf@gmail.com>
2021-08-13 21:53:22 -0300

    Refatoraçao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 //using Telegram.Bot.Extensions.Polling;
@@ -7,17 +8,15 @@ namespace bot_csharp_telegram
 {
     class Program
     {
-        static ITelegramBotClient botClient;
-        static void Main()
+        private static TelegramBotClient botClient = new TelegramBotClient("YOUR_TOKEN"); // YOUR_TOKEN
+        static void Main(string[] args)
         {
-            botClient = new TelegramBotClient("YOUR_TOKEN");
-
             var me = botClient.GetMeAsync().Result;
             Console.WriteLine($"Olá! Eu sou o {me.FirstName}.");
 
             botClient.OnMessage += Bot_OnMessage;
             botClient.StartReceiving();
-
+            Thread.Sleep(Timeout.Infinite);
             Console.WriteLine("Pressione qualquer tecla para sair...");
             Console.ReadKey();
 
@@ -31,8 +30,10 @@ namespace bot_csharp_telegram
                 Console.WriteLine($"Mensagem recebida do chat {e.Message.Chat.Id}.");
 
                 await botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: "Você disse:\n " + e.Message.Text
+                    // chatId: e.Message.Chat,
+                    // text: "Você disse:\n " + e.Message.Text
+                    e.Message.Chat.Id,
+                    $"Olá {e.Message.From.FirstName}, tudo bem? Você disse: \n {e.Message.Text}"
                 );
             }
         }
